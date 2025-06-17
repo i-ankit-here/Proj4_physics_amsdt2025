1e0c4b31fa8970bc6c095805c86df32d12e20bfc
Dr. D. Harimurugan <88608850+harimurugan1989@users.noreply.github.com>
2025-06-17 21:32:43 +0530

    Update RegistrationLink.jsx

--- src/pages/Registration/RegistrationLink.jsx
+++ src/pages/Registration/RegistrationLink.jsx
@@ -40,10 +40,7 @@ function RegistrationLink() {
                                     rel="noopener noreferrer"
                                     className="relative flex text-blue-900 items-center justify-center px-8 py-4 bg-white border border-blue-900 shadow-lg rounded-lg leading-none font-medium hover:bg-blue-900 hover:text-red-600 transition-colors duration-300"
                                 >
-                                    <span className="mr-3">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-blue-900">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 0 1 0 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 0 1 0-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375Z" />
-                                        </svg>
+                               
                                     </span>
                                     Register for AMSDT-2025
                                 </a>
